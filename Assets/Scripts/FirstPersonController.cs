ca28bcb3243ae48991eaabaa77f87c73936f86a1
raoul <sshpcl2@gmail.com>
2021-05-25 15:38:49 +0200

    modification rotate remote controller with mouse

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -12,51 +12,61 @@ public class FirstPersonController : MonoBehaviour
     [SerializeField] private Transform cameraRef; 
 
     Quaternion cameraRot;
+    Transform cameraTransform; 
     Quaternion characterRot;
+
+    float Xsensitivity = 2;
+    float Ysensitivity = 2;
+
+    float MinimumX = -90;
+    float MaximumX = 90; 
+
+
 
 
 
     private void Start()
     {
-        cameraRot = cam.transform.localRotation;
-        characterRot = character.transform.localRotation;
+        
        
+        //cameraRot = cam.transform.localRotation;
+        cameraTransform = cam.transform;
+        characterRot = character.transform.localRotation;
+
     }
 
     void Update()
     {
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
 
-        float yRot = Input.GetAxis("Mouse X");
-        float xRot = Input.GetAxis("Mouse Y");
+        //the camera is NOT moving its the remote command only 
+        float yRot = Input.GetAxis("Mouse Y") * Ysensitivity;
+        float xRot = Input.GetAxis("Mouse X") * Xsensitivity;
+        //cameraRot *= Quaternion.Euler(-xRot, 0, 0);
+        characterRot *= Quaternion.Euler(0, xRot, yRot);
 
-        cameraRot *= Quaternion.Euler(-xRot , yRot, 0);
-        characterRot *= Quaternion.Euler(0, yRot , 0);
-
-        this.transform.localRotation = characterRot;
-        cam.transform.localRotation = cameraRot; 
+        //cam.transform.localRotation = cameraRot; 
+        character.transform.localRotation = characterRot; 
 
         MovePlayer();
 
     }
 
+
+
     private void MovePlayer()
     {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * speed ;// rotationSpeed;
+        float v = Input.GetAxis("Vertical") * speed;
+        float h = Input.GetAxis("Horizontal") * speed ;// rotationSpeed;
 
-        Vector3 camForward = Vector3.Scale(cameraRef.forward, new Vector3(1, 0, 1)).normalized;
-        Vector3 moveVector = (translation * camForward + rotation * cameraRef.right).normalized * speed * Time.deltaTime;
+        //Vector3 camForward = Vector3.Scale(cameraRef.forward, new Vector3(1, 0, 1)).normalized;
+        //print("cam forward is " + camForward);
+        //Vector3 moveVector = (translation * camForward + rotation * cameraRef.right).normalized * speed * Time.deltaTime;
+        //print("move vector is " + moveVector);
+        //transform.Translate(moveVector);
 
-        transform.Translate(moveVector);
-
-        /*
-          float translation = Input.GetAxis("Vertical") * speed;
-          float rotation = Input.GetAxis("Horizontal") * speed ;// rotationSpeed;
-          transform.Translate(translation * Time.deltaTime * speed, 0, rotation * Time.deltaTime, Space.World);
-         */
+          Vector3 move = new Vector3(v * Time.deltaTime * speed, 0, h* Time.deltaTime * cameraTransform.transform.right.z) ; 
+          transform.Translate(move);
+         
         // transform.Rotate(0, rotation * Time.deltaTime, 0);
 
     }
